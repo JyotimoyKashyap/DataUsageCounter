86a76a9bfc7a484e998514f7282637981357896d
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2021-09-27 15:10:29 +0530

    initial commit'

---
+++ app/src/main/java/com/jyotimoykashyap/customwidget/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.jyotimoykashyap.customwidget
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
+import com.jyotimoykashyap.customwidget.ui.theme.CustomWidgetTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomWidgetTheme {
+                
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    var value by remember {
+                        mutableStateOf(0)
+                    }
+
+                    Spacer(
+                        modifier = Modifier
+                            .size(height = 200.dp, width = 200.dp))
+
+                    CustomComponent(
+                        indicatorValue = value
+                    )
+
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if(it.isNotEmpty() ){
+                                it.toInt()
+                            }else{
+                                0
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+
+}
